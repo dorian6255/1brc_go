1b6e1e66db06e34aa928b53e4daa8fc27d0c281d
Dorian <dorian.6255@hotmail.fr>
2024-04-21 08:49:02 +0200

    use pointer for float

--- src/main/go/dorian6255_1brc/main.go
+++ src/main/go/dorian6255_1brc/main.go
@@ -1,136 +1,79 @@
 package main
 
 import (
-	"bufio"
+	"dorian6255_1brc/internals/dataType"
+	"dorian6255_1brc/internals/handling"
+	"dorian6255_1brc/internals/reading"
 	"fmt"
 	"os"
-	"runtime/pprof"
-	"runtime/trace"
 	"sort"
-	"strconv"
-	"strings"
-	"sync"
-	// "text/scanner"
 )
 
-var wgread sync.WaitGroup
-
-var wgprocess sync.WaitGroup
-
-var wgstore sync.WaitGroup
-
-const batchSize = 1000
-
-const nbReader = 10
-const endlineSymbol byte = '\n'
-const bufferSize = 35
-const uniqueStationName = 10000
-
-// my plan is to divide the file by x goroutine  -3
-// 1 goroutine will be allocated to find the min value of each one
-// another one will be allocated to find the max value of each one
-// another one will be allocated to find the avg value of each one
-// i'll have to try if one goroutine can handle all 3 of it / if it's faster
-// each one will be given a part of the file to read
-// we store everything in ram, a map[NAME][]VALUES
-// once we fully reach the end of the file, we must find the avg of each one
-// then we return the result
-type valuesOut struct {
-	Name string
-	Min  float32
-	Max  float32
-	Avg  float32
-}
-type storeUnit struct {
-	name  string
-	value float32
-}
-
-type valueIn struct {
-	name  string
-	value float32
-}
+// optimisations idea :
+// string build to return the output in 1 time
+// either pass float value as pointer or create my own type of float to reduce size because value are between -99.00 && 99.99 in worst case scenario to reduce copy time
+// remove Name from values out
+// store []byte instead of string ?
+//  pass struct as reference
 
 func main() {
-	minimum := make(map[string]float32, uniqueStationName)
-	maximum := make(map[string]float32, uniqueStationName)
-	average := make(map[string]float32, uniqueStationName)
-	nb := make(map[string]float32, uniqueStationName)
+	minimum := make(map[string]*float32, dataType.UniqueStationName)
+	maximum := make(map[string]*float32, dataType.UniqueStationName)
+	average := make(map[string]*float32, dataType.UniqueStationName)
+	nb := make(map[string]*float32, dataType.UniqueStationName)
 	// maximum := sync.Map{}
 	// average := sync.Map{}
 	// nb := sync.Map{}
 	//
-	f, err := os.Create("profile.prof")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	// Start CPU profiling
-	if err := pprof.StartCPUProfile(f); err != nil {
-		panic(err)
-	}
-	defer pprof.StopCPUProfile()
-	// Start tracing
-	traceFile, err := os.Create("trace.out")
-	if err != nil {
-		panic(err)
-	}
-	defer traceFile.Close()
-
-	if err := trace.Start(traceFile); err != nil {
-		panic(err)
-	}
-	defer trace.Stop()
 	//start
 	// var res = sync.Map{}
 
 	filename := os.Args[1]
-	content, err := os.ReadFile(filename)
-	lines := make(chan [bufferSize]byte, batchSize)
-	in := make(chan valueIn, batchSize)
+	content, _ := os.ReadFile(filename)
+	lines := make(chan [dataType.BufferSize]byte, dataType.BatchSize)
+	in := make(chan dataType.ValueIn, dataType.BatchSize)
 
-	max := make(chan storeUnit, batchSize)
-	min := make(chan storeUnit, batchSize)
-	avg := make(chan storeUnit, batchSize)
+	max := make(chan dataType.StoreUnit, dataType.BatchSize)
+	min := make(chan dataType.StoreUnit, dataType.BatchSize)
+	avg := make(chan dataType.StoreUnit, dataType.BatchSize)
 
-	go sendScanBuf(content, lines)
+	go reading.SendScanBuf(content, lines)
 
-	wgread.Add(1)
+	dataType.Wgread.Add(1)
 
 	for i := 0; i < 1; i++ {
 
-		go processLine(lines, in)
+		go reading.ProcessLine(lines, in)
 
-		wgprocess.Add(1)
+		dataType.Wgprocess.Add(1)
 	}
 
-	go handleAvg(avg, average, nb)
+	go handling.HandleAvg(avg, average, nb)
 
-	wgstore.Add(1)
-	go handleMax(max, maximum)
+	dataType.Wgstore.Add(1)
+	go handling.HandleMax(max, maximum)
 
-	wgstore.Add(1)
-	go handleMin(min, minimum)
+	dataType.Wgstore.Add(1)
+	go handling.HandleMin(min, minimum)
 
-	wgstore.Add(1)
+	dataType.Wgstore.Add(1)
 
-	go handleValues(in, min, avg, max)
-	wgstore.Add(1)
+	go handling.HandleValues(in, min, avg, max)
+	dataType.Wgstore.Add(1)
 
-	wgread.Wait()
+	dataType.Wgread.Wait()
 	close(lines)
 
-	wgprocess.Wait()
+	dataType.Wgprocess.Wait()
 
 	close(in)
 
-	wgstore.Wait()
+	dataType.Wgstore.Wait()
 
-	resmap := make(map[string]valuesOut, uniqueStationName)
+	resmap := make(map[string]dataType.ValuesOut, dataType.UniqueStationName)
 
 	for key, value := range maximum {
-		resmap[key] = valuesOut{Name: key, Max: value}
+		resmap[key] = dataType.ValuesOut{Name: key, Max: value}
 	}
 	// maximum.Range(func(key, value any) bool {
 	// 	k, _ := key.(string)
@@ -184,158 +127,13 @@ func main() {
 		value := resmap[str]
 		if !i {
 
-			fmt.Printf("%v=%.2f/%.2f/%.2f", value.Name, value.Min, value.Avg, value.Max)
+			fmt.Printf("%v=%.2f/%.2f/%.2f", value.Name, *value.Min, *value.Avg, *value.Max)
 			i = true
 		} else {
 
-			fmt.Printf(", %v=%.2f/%.2f/%.2f", value.Name, value.Min, value.Avg, value.Max)
+			fmt.Printf(", %v=%.2f/%.2f/%.2f", value.Name, *value.Min, *value.Avg, *value.Max)
 		}
 	}
 	fmt.Print("}")
 
-}
-
-func sendScanBuf(data []byte, lines chan [bufferSize]byte) {
-
-	// read_lines := strings.Split(string(data), "\n")
-	// read_lines = read_lines[:len(read_lines)-1]
-	var buffer [bufferSize]byte
-	for i := 0; i < len(data); i++ {
-		//form line
-		j := 0
-		for ; i+j < len(data) && data[i+j] != endlineSymbol; j++ {
-			buffer[j] = data[i+j]
-		}
-
-		i += j
-
-		lines <- buffer
-	}
-
-	wgread.Done()
-	return
-
-}
-func sendScan(scanner *bufio.Scanner, lines chan string) {
-
-	// read_lines := strings.Split(string(*data), "\n")
-	//
-	// read_lines = read_lines[:len(read_lines)-1]
-	//
-	// for _, line := range read_lines {
-	// 	lines <- line
-	// }
-	for scanner.Scan() {
-
-		lines <- scanner.Text()
-	}
-
-	wgread.Done()
-	return
-
-}
-
-func handleMax(in chan storeUnit, maximum map[string]float32) {
-
-	for elem := range in {
-		max, ok := maximum[elem.name]
-
-		// f, _ := max.(float32)
-
-		if !ok || elem.value > max {
-			maximum[elem.name] = elem.value
-
-		}
-	}
-
-	wgstore.Done()
-	return
-}
-func handleAvg(in chan storeUnit, average, number map[string]float32) {
-
-	for elem := range in {
-		nb, ok := number[elem.name]
-
-		if ok {
-
-			prevAvg, _ := average[elem.name]
-
-			value := ((prevAvg * nb) + elem.value) / (nb + 1)
-			average[elem.name] = value
-		} else {
-			number[elem.name] = 1.0
-
-			average[elem.name] = elem.value
-		}
-	}
-
-	wgstore.Done()
-	return
-}
-func handleMin(in chan storeUnit, minimum map[string]float32) {
-
-	for elem := range in {
-		// min, ok := minimum.LoadOrStore(elem.name, elem.value)
-
-		min, ok := minimum[elem.name]
-
-		if !ok || elem.value < min {
-			minimum[elem.name] = elem.value
-
-		}
-	}
-
-	wgstore.Done()
-	return
-}
-
-func handleValues(in chan valueIn, minimum, average, maximum chan storeUnit) {
-
-	for elem := range in {
-		minimum <- storeUnit{name: elem.name, value: elem.value}
-		maximum <- storeUnit{name: elem.name, value: elem.value}
-		average <- storeUnit{name: elem.name, value: elem.value}
-	}
-	close(minimum)
-	close(maximum)
-	close(average)
-	wgstore.Done()
-}
-
-func processLine(lines chan [bufferSize]byte, in chan valueIn) {
-
-	var group sync.WaitGroup
-	batch := make([][bufferSize]byte, batchSize)
-	i := 0
-	for newLine := range lines {
-
-		batch[i] = newLine
-
-		i++
-		if i == batchSize {
-			batchCopy := batch
-			group.Add(1)
-			go func() {
-				for y := 0; y < i; y++ {
-
-					splitLine := strings.Split(string(batchCopy[y][:]), ";")
-
-					name := splitLine[0]
-
-					value, _ := strconv.ParseFloat(splitLine[1], 32)
-					in <- valueIn{name: name, value: float32(value)}
-
-				}
-				group.Done()
-				return
-			}()
-			i = 0
-
-		}
-
-	}
-
-	group.Wait()
-	wgprocess.Done()
-	return
 }
