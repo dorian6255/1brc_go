716f48d4303c42c28877cf442d71289af092f3e0
Dorian <dorian.6255@hotmail.fr>
2024-04-27 23:04:40 +0200

    finish split and merge

--- src/main/go/dorian6255_1brc/v2/main.go
+++ src/main/go/dorian6255_1brc/v2/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 	"strconv"
 )
 
@@ -21,10 +22,11 @@ func main() {
 }
 
 type outputType struct {
-	name string
-	min  int
-	max  int
-	avg  int
+	// name string
+	Min int
+	Max int
+	Avg int
+	Nb  int
 }
 
 // INFO: i did not include test because of how simple the function is, and the lack of opporunity i can see to improve the efficiency
@@ -39,39 +41,98 @@ func loadFile(filename string) []byte {
 
 }
 
-// TODO:
+// PERF: regarding the size of the function, i do not see how i can improve this one
+// Used to split the file at the right place and not in the middle on an entry
+// given a content, we return the idx of the latest endlinesymbole that we can find
+func findNextEndline(content []byte) int {
+
+	i := len(content) - 1
+	for ; i > 0 && content[i] != endlineSymbole; i-- {
+
+	}
+	return i
+}
+
+// INFO: the res := make(..... improve the performance by 2 accordings to bench !!
 // Given a file as content, we want this function to return X arrays of byte
 // X is gonna be the number of thread on the cpu
 // the goal here is to process the file multiple part at a time
 func splitContent(content []byte) [][]byte {
+	res := make([][]byte, runtime.NumCPU())
+	splitSize := len(content) / runtime.NumCPU()
 
-	return nil
-}
+	idxStart := 0
+	idxEnd := findNextEndline(content[0:splitSize])
+	for i := 0; i < runtime.NumCPU(); i++ {
 
-// TODO:
-func mergeResult(data ...map[string]outputType) map[string]outputType {
-
-	return nil
-}
-
-// TODO:
-func process(data []byte) map[string]outputType {
-
-	var tmp [maxSizeName]byte
-	for i := 0; i < maxSizeName; i++ {
-		tmp[i] = data[i]
+		res[i] = content[idxStart:idxEnd]
+		idxStart = idxEnd + 1
+		newEnd := idxEnd + splitSize
+		if newEnd > len(content) {
+			newEnd = len(content)
+		}
+		idxEnd = idxEnd + findNextEndline(content[idxEnd:newEnd])
 
 	}
-	fmt.Println(tmp)
-	fmt.Println(string(tmp[:]))
-	var b byte = '\n'
-	var c byte = ';'
-	fmt.Println(b)
-	fmt.Println(c)
-	return nil
+
+	return res
 }
 
-// TODO: for now it take ~34 ns per operation, maybe we can improve some stuf : reduce size int?, byte size ? paralell ?
+// receive runtime.numcp() map and merge the result of each map into a res map
+func mergeResult(data ...map[string]outputType) map[string]outputType {
+	//TODO: use make to see perrformance evolution
+	//and try to use the first map
+	var res = map[string]outputType{}
+
+	for _, m := range data {
+
+		for k, v := range m {
+			value, ok := res[k]
+			if ok {
+				switch {
+				//TODO: make a function that take two outputType, and return one with min,max,avg, updated
+				case value.Min > v.Min:
+					value.Min = v.Min
+				case value.Max < v.Max:
+					value.Max = v.Max
+
+				}
+				//NOTE: for avg and nb
+
+				value.Avg = ((value.Avg * value.Nb) + (v.Avg * v.Nb)) / (value.Nb + v.Nb)
+				value.Nb += v.Nb
+
+				res[k] = value
+			} else {
+				res[k] = v
+			}
+
+		}
+	}
+
+	return res
+}
+
+// TODO:
+// process take a part of the file in params
+// parse each line
+// fill a map
+// return the map
+// it will run in a go routine
+func process(data []byte) map[string]outputType {
+
+	res := make(map[string]outputType, maxDifferentName)
+
+	//TODO: process line by line
+	//for the size of the data
+	// if we find an endline
+	// we get the name, value of the line
+	// we process the result (min, max, avg)
+	// we go to the next one
+	return res
+}
+
+// INFO:: for now it take ~34 ns per operation, maybe we can improve some stuf : reduce size int?, byte size ? paralell ?
 func interpretLine(line []byte) ([]byte, int) {
 	lineIdx := 0
 	//handle name
--- src/main/go/dorian6255_1brc/v2/main_test.go
+++ src/main/go/dorian6255_1brc/v2/main_test.go
@@ -1,11 +1,59 @@
 package main
 
 import (
+	"fmt"
 	"reflect"
 	"runtime"
 	"testing"
 )
 
+func Test_findNextEndline(t *testing.T) {
+	type args struct {
+		content []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "test simple case", args: args{[]byte{48, 49, endlineSymbole, 58, 47, 53}}, want: 2},
+		{name: "test is latest", args: args{[]byte{48, 49, endlineSymbole}}, want: 2},
+		{name: "test is first", args: args{[]byte{endlineSymbole, 48, 49}}, want: 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// showResult(tt.args.res)
+			got := findNextEndline(tt.args.content)
+			if got != tt.want {
+				t.Errorf("Expected to find endline at %v, found at %v", tt.want, got)
+
+			}
+		})
+	}
+
+}
+
+// func BenchmarkInterpretLine(b *testing.B) {
+func BenchmarkSplitContent1M(b *testing.B) {
+
+	type args struct {
+		content []byte
+	}
+	type testParams struct {
+		name string
+		args args
+	}
+	testFiles := "tests/measurements1000000.txt"
+
+	content := loadFile(testFiles)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+
+		splitContent(content)
+
+	}
+
+}
 func Test_splitContent(t *testing.T) {
 
 	type args struct {
@@ -16,7 +64,7 @@ func Test_splitContent(t *testing.T) {
 		args args
 	}
 	var testFolder string = "tests/"
-	testFiles := [5]string{testFolder + "test12.txt", testFolder + "test24.txt", testFolder + "test48.txt", testFolder + "test96.txt", testFolder + "test100.txt"}
+	testFiles := [5]string{testFolder + "test24.txt", testFolder + "test48.txt", testFolder + "test96.txt", testFolder + "test100.txt", testFolder + "measurements1000000.txt"}
 
 	var tests []testParams
 	var content [][]byte
@@ -28,6 +76,8 @@ func Test_splitContent(t *testing.T) {
 
 	//check if split in 12
 	//check if last if \n
+	const zero byte = 48
+	const nine byte = 57
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 
@@ -35,18 +85,22 @@ func Test_splitContent(t *testing.T) {
 			if len(got) != runtime.NumCPU() {
 				t.Errorf("split not using all cpu, got: %v", len(got))
 			}
-			for _, split := range content {
 
-				if split[len(split)-1] != endlineSymbole {
-					t.Errorf("Last symbole in not endlineSymbole %v", split[len(split)-1])
+			for _, split := range got {
+
+				if split[len(split)-1] < zero || split[len(split)-1] > nine {
+					t.Errorf("Last symbole is not a number as it should %s, %s", split[len(split)-1:], split[:])
 				}
+				// fmt.Println(split[len(split)-1:])
+
+				// fmt.Println(string(zero))
 			}
 
 		})
 	}
 }
 
-func Test_mergeResult(t *testing.T) {
+func Test_mergeMinMaxAvg(t *testing.T) {
 	type args struct {
 		data []map[string]outputType
 	}
@@ -55,13 +109,92 @@ func Test_mergeResult(t *testing.T) {
 		args args
 		want map[string]outputType
 	}{
-		// TODO: Add test cases.
+		{name: "testOneMap", args: args{[]map[string]outputType{map[string]outputType{"toto1": outputType{1, 1, 1, 1}}}}, want: map[string]outputType{"toto1": outputType{1, 1, 1, 1}}},
+		{name: "testTwoMapAvg", args: args{[]map[string]outputType{map[string]outputType{"toto1": outputType{1, 1, 10, 1}}, map[string]outputType{"toto1": outputType{1, 1, 20, 1}}}}, want: map[string]outputType{"toto1": outputType{1, 1, 15, 2}}},
+		{name: "testTwoMapMin", args: args{[]map[string]outputType{map[string]outputType{"toto1": outputType{2, 1, 10, 1}}, map[string]outputType{"toto1": outputType{5, 1, 20, 1}}}}, want: map[string]outputType{"toto1": outputType{2, 1, 15, 2}}},
+		{name: "testTwoMapMax", args: args{[]map[string]outputType{map[string]outputType{"toto1": outputType{1, 2, 10, 1}}, map[string]outputType{"toto1": outputType{1, 5, 20, 1}}}}, want: map[string]outputType{"toto1": outputType{1, 5, 15, 2}}},
+		{name: "testWithMoreComplexCase1", args: args{[]map[string]outputType{map[string]outputType{"toto1": outputType{-120, 50, 150, 15}}, map[string]outputType{"toto1": outputType{0, 200, 25, 7}}}}, want: map[string]outputType{"toto1": outputType{-120, 200, 110, 22}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := mergeResult(tt.args.data...); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("mergeResult() = %v, want %v", got, tt.want)
+			got := mergeResult(tt.args.data...)
+			tmpGot := got["toto1"]
+			tmpWant := tt.want["toto1"]
+			if tmpGot.Min != tmpWant.Min {
+				t.Errorf("Min Value incorrect got %v, want %v", got["toto1"].Min, tt.want["toto1"].Min)
 			}
+			if tmpGot.Max != tmpWant.Max {
+				t.Errorf("Max Value incorrect got %v, want %v", got["toto1"].Max, tt.want["toto1"].Max)
+			}
+			if tmpGot.Avg != tmpWant.Avg {
+				t.Errorf("Avg Value incorrect got %v, want %v", got["toto1"].Avg, tt.want["toto1"].Avg)
+			}
+			if tmpGot.Nb != tmpWant.Nb {
+				t.Errorf("NB Value incorrect got %v, want %v", got["toto1"].Nb, tt.want["toto1"].Nb)
+			}
+		})
+	}
+
+}
+func Test_mergeResult(t *testing.T) {
+
+	type args struct {
+		data []map[string]outputType
+	}
+	type test = struct {
+		name string
+		args args
+		want map[string]outputType
+	}
+	var tests []test
+
+	//NOTE: want creation
+	// for the test, i just use client{{i}}= i,i,i as value that i need to find
+	want := map[string]outputType{}
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+
+		want[fmt.Sprintf("client%v", i)] = outputType{i, i, i, 1}
+	}
+
+	//NOTE: args creation
+	// I test with 2,3,4,5 ... 12 maps for the sake of it
+	for i := 2; i < runtime.NumCPU(); i++ {
+
+		var tmp = test{}
+		tmp.name = fmt.Sprintf("test with %v", i)
+		var tmpArgs = args{}
+		for nbMap := 0; nbMap < i; nbMap++ {
+
+			mapSplit := runtime.NumCPU() / i
+
+			for x := 0; x < runtime.NumCPU(); x += mapSplit {
+
+				var tmpMap = map[string]outputType{}
+
+				for j := 0; j < mapSplit; j++ {
+					if j < mapSplit {
+						tmpMap[fmt.Sprintf("client%v", j+x)] = outputType{j + x, j + x, j + x, 1}
+					}
+
+				}
+				tmpArgs.data = append(tmpArgs.data, tmpMap)
+			}
+		}
+		tmp.args = tmpArgs
+
+		tmp.want = want
+		tests = append(tests, tmp)
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got := mergeResult(tt.args.data...)
+			if len(got) != len(tt.want) {
+				t.Errorf("incorrect number of entry receive, got %v, want %v", got, tt.want)
+			}
+
 		})
 	}
 }
@@ -100,11 +233,11 @@ func BenchmarkInterpretLine(b *testing.B) {
 		{name: "test1Case", args: args{data: []byte{84, 111, 97, 109, 97, 115, 105, 110, 97, 59, 50, 50, 46, 48, 10}}, want: []byte{84, 111, 97, 109, 97, 115, 105, 110, 97}, want1: 220},
 		{name: "test2Case", args: args{data: []byte{84, 101, 108, 32, 65, 118, 105, 118, 59, 50, 54, 46, 55, 10}}, want: []byte{84, 101, 108, 32, 65, 118, 105, 118}, want1: 267},
 	}
-	for _, tt := range tests {
+	for idx, tt := range tests {
 		b.Run(tt.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 
-				interpretLine(tests[0].args.data)
+				interpretLine(tests[idx].args.data)
 
 			}
 		})
